898a4ff93cbeb92748f014d289ba42b458ca3633
lizheng <li396858359@163.com>
2015-08-24 10:45:11 +0800

    code fix

--- src/main/java/com/hkllzh/android/net/package-info.java
+++ src/main/java/com/hkllzh/android/net/package-info.java
@@ -3,9 +3,9 @@
  * <b>请求地址（url）</b><br>
  * <b>请求方法（get/post）</b><br>
  * <b>请求参数（params）</b><br>
- * 本包基于此三个输入项，通过{@link com.squareup.okhttp.OkHttpClient}实现网络请求功能
- *
- * <p></p>
+ * 本包基于此三个输入项，实现一个通用的网络请求模型。<br/>
+ * 在okhttp包里面通过{@link com.squareup.okhttp.OkHttpClient}实现真实地网络请求功能
+ * <p/>
  * 需要可以直接使用，同时可定制使用。
  */
 package com.hkllzh.android.net;
