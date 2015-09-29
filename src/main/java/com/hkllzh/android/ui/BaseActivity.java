b096ed262e1c0880f9e152ccb20eb21d78823894
lizheng <li396858359@163.com>
2015-09-29 17:58:30 +0800

    修复bug

--- src/main/java/com/hkllzh/android/ui/BaseActivity.java
+++ src/main/java/com/hkllzh/android/ui/BaseActivity.java
@@ -19,19 +19,21 @@ import com.hkllzh.android.util.sharedpreferences.SPUtil;
 public class BaseActivity extends AppCompatActivity {
 
     private static final LogInterface _log;
-    protected static final SPUtil spUtil;
-    protected static final int W_PX;
-    protected static final int H_PX;
+    private static final SPUtil _spUtil;
+    private static final int _W_PX;
+    private static final int _H_PX;
 
     static {
         _log = LogHandler.getInstance();
-        spUtil = SPUtil.getInstance();
-        W_PX = spUtil.getInt(C.SP.SCREEN_WIDTH, 0);
-        H_PX = spUtil.getInt(C.SP.SCREEN_HEIGHT, 0);
+        _spUtil = SPUtil.getInstance();
+        _W_PX = _spUtil.getInt(C.SP.SCREEN_WIDTH, 0);
+        _H_PX = _spUtil.getInt(C.SP.SCREEN_HEIGHT, 0);
     }
 
     protected LogInterface log;
-
+    protected SPUtil spUtil;
+    protected int W_PX;
+    protected int H_PX;
     protected Activity mActivity;
 
     @Override
@@ -39,5 +41,8 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mActivity = this;
         log = _log;
+        spUtil = _spUtil;
+        W_PX = _W_PX;
+        H_PX = _H_PX;
     }
 }
